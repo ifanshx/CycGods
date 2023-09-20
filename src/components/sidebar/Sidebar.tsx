35715b3f49dcd367b4fc7705b927dde3e3687d20
ifanshx <irfaanshoodiq1954@gmail.com>
2023-09-21 02:25:21 +0700

    update sidebar after login

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -15,6 +15,7 @@ import {
 } from "@heroicons/react/24/solid";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 type Props = {
   showNav: boolean;
@@ -78,6 +79,7 @@ const isActivePath = (path: string, currentPath: string) =>
 // eslint-disable-next-line react/display-name
 export const Sidebar = forwardRef<HTMLElement, Props>(({ showNav }, ref) => {
   const router = useRouter();
+  const { data }: any = useSession();
 
   return (
     <aside ref={ref} className="fixed z-30 w-56 h-full bg-white shadow-sm p-4">
@@ -86,28 +88,35 @@ export const Sidebar = forwardRef<HTMLElement, Props>(({ showNav }, ref) => {
       </div>
 
       <ul className="space-y-3">
-        {MENU_ITEMS.map((category) => (
-          <li key={category.category}>
-            <div className={CATEGORY_TITLE_STYLING}>{category.category}</div>
-            <ul>
-              {category.items.map(({ name, icon: Icon, path }) => (
-                <li key={name.toLowerCase().replace(" ", "-")}>
-                  <Link
-                    href={path}
-                    className={`block pl-4 py-2 cursor-pointer flex items-center gap-2 transition-colors ease-in-out duration-150 ${HOVER_STYLING} ${
-                      isActivePath(path, router.pathname)
-                        ? ACTIVE_STYLING
-                        : "text-gray-600"
-                    }`}
-                  >
-                    <Icon className="h-5 w-5" />
-                    <span>{name}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+        {MENU_ITEMS.map((category) => {
+          // Conditionally render the "My Account" category if the user is logged in
+          if (category.category === "My Account" && !data) {
+            return null; // Skip rendering
+          }
+
+          return (
+            <li key={category.category}>
+              <div className={CATEGORY_TITLE_STYLING}>{category.category}</div>
+              <ul>
+                {category.items.map(({ name, icon: Icon, path }) => (
+                  <li key={name.toLowerCase().replace(" ", "-")}>
+                    <Link
+                      href={path}
+                      className={`block pl-4 py-2 cursor-pointer flex items-center gap-2 transition-colors ease-in-out duration-150 ${HOVER_STYLING} ${
+                        isActivePath(path, router.pathname)
+                          ? ACTIVE_STYLING
+                          : "text-gray-600"
+                      }`}
+                    >
+                      <Icon className="h-5 w-5" />
+                      <span>{name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
